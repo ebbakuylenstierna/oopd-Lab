1b125dc93c7b542bbd9147d51c4bf729342205ea
Oli <oli@olillin.com>
2025-02-11 13:55:44 +0100

    Rename ScaniaTruck.java to Scania.java

--- src/main/lab/car/Scania.java
+++ src/main/lab/car/Scania.java
@@ -5,8 +5,8 @@ import lab.ramp.IAngledRamp;
 
 import java.awt.*;
 
-public class ScaniaTruck extends CarWithRamp<IAngledRamp> implements IAngledRamp {
-    public ScaniaTruck() {
+public class Scania extends CarWithRamp<IAngledRamp> implements IAngledRamp {
+    public Scania() {
         super(2, Color.BLUE, 200, "Scania", new AngledRamp(0, 70));
     }
 
